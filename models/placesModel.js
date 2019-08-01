2d95a004b235c82c2072eb5d3c4a4ae179ae1568
creativeBernadka <bfryczkowska@gmail.com>
2019-08-01 12:36:48 +0200

    FIX: config vars added
    Variables from config file are now updated
    to heroku standards.

--- models/placesModel.js
+++ models/placesModel.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql');
-const config = require('../config');
 
 let connection = mysql.createConnection({
-    host: config.database.host,
-    user: config.database.user,
-    password: config.database.password,
-    database: config.database.database
+    host: process.env.HOST,
+    user: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE_NAME
 });
 connection.connect(function(err) {
     if (err)throw err;
