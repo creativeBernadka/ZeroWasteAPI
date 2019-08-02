713795251d7bfd58fb4e9a0a7c125fab090e6e6c
creativeBernadka <bfryczkowska@gmail.com>
2019-08-02 08:49:42 +0200

    FIX: connection changed to pool

--- models/placesModel.js
+++ models/placesModel.js
@@ -1,14 +1,15 @@
 const mysql = require('mysql');
 
-let connection = mysql.createConnection({
+let connection = mysql.createPool({
+    connectionLimit: 10,
     host: process.env.HOST_NAME,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE_NAME
 });
-connection.connect(function(err) {
-    if (err)throw err;
-    console.log("Connected! Jupi jej!");
-});
+// connection.connect(function(err) {
+//     if (err) throw err;
+//     console.log("Connected! Jupi jej!");
+// });
 
 module.exports = connection;
